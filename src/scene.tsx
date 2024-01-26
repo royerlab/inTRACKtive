671301a80fbbaaa0787f6a8d8947eb608b53102c
Andy Sweet <andrew.d.sweet@gmail.com>
2024-01-26 13:58:54 -0800

    Move three canvas to its own module

--- src/scene.tsx
+++ src/scene.tsx
@@ -1,13 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { InputSlider, InputText, InputToggle } from "@czi-sds/components";
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
-import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
-import { SelectionHelper } from 'three/addons/interactive/SelectionHelper.js';
-import { PointSelectionBox } from './PointSelectionBox';
+import { Canvas } from './canvas';
 
 // @ts-expect-error
 import { ZarrArray, slice, openArray } from "zarr";
@@ -15,193 +8,6 @@ import { ZarrArray, slice, openArray } from "zarr";
 
 const DEFAULT_ZARR_URL = new URL("https://public.czbiohub.org/royerlab/zebrahub/imaging/single-objective/tracks_benchmark/ZSNS001_nodes.zarr");
 
-class Canvas {
-    renderer: THREE.WebGLRenderer
-    points: THREE.Points
-    composer: EffectComposer
-    controls: OrbitControls
-    bloomPass: UnrealBloomPass
-    selectionBox: PointSelectionBox
-    selectionHelper: SelectionHelper
-
-    constructor(width: number, height: number) {
-        const aspect = width / height;
-
-        const scene = new THREE.Scene();
-        this.renderer = new THREE.WebGLRenderer();
-
-        const camera = new THREE.PerspectiveCamera(
-            35,             // FOV
-            aspect,         // Aspect
-            0.1,            // Near
-            10000           // Far
-        );
-
-        // Default position from interacting with ZSNS001
-        // TODO: this should be set/reset when the data changes
-        const target = new THREE.Vector3(500, 500, 250);
-        camera.position.set(target.x, target.y, target.z - 1500);
-        camera.lookAt(target.x, target.y, target.z);
-
-        const geometry = new THREE.BufferGeometry();
-        const material = new THREE.PointsMaterial(
-            {
-                size: 16.0,
-                map: new THREE.TextureLoader().load("/spark1.png"),
-                vertexColors: true,
-                blending: THREE.AdditiveBlending,
-                depthTest: false,
-                transparent: true,
-            }
-        );
-        this.points = new THREE.Points(geometry, material);
-
-        scene.add(new THREE.AxesHelper(128));
-        scene.add(this.points);
-        scene.fog = new THREE.FogExp2(0x000000, 0.0005);  // default is 0.00025
-
-        // Effect composition.
-        const renderModel = new RenderPass(scene, camera);
-        this.bloomPass = new UnrealBloomPass(
-            new THREE.Vector2(width, height), // resolution
-            0.4, // strength
-            0, // radius
-            0  // threshold
-        );
-        const outputPass = new OutputPass();
-        this.composer = new EffectComposer(this.renderer);
-        this.composer.addPass(renderModel);
-        this.composer.addPass(this.bloomPass);
-        this.composer.addPass(outputPass);
-
-        // Point selection
-        this.selectionHelper = new SelectionHelper(this.renderer, 'selectBox');
-        this.selectionHelper.enabled = false;
-        this.selectionBox = new PointSelectionBox(camera, scene);
-        // TODO: improve the behavior when pressing/releasing the mouse and
-        // shift key in different orders
-        this.renderer.domElement.addEventListener('pointerup', this.pointerUp);
-
-        // TODO: add clean-up by returning another closure
-        // Set up controls
-        this.controls = new OrbitControls(camera, this.renderer.domElement);
-        this.controls.target.set(target.x, target.y, target.z);
-        this.controls.autoRotateSpeed = 1;
-    }
-
-    animate = () => {
-        requestAnimationFrame(this.animate);
-        // Render the scene
-        this.composer.render();
-        this.controls.update();
-    }
-
-    pointerUp = () => {
-        console.log("pointerUp: %s", this.selectionHelper.enabled);
-        if (this.selectionHelper && this.selectionHelper.enabled) {
-            // Mouse to normalized render/canvas coords from:
-            // https://codepen.io/boytchev/pen/NWOMrxW?editors=0011
-            const canvas = this.renderer.domElement.getBoundingClientRect();
-
-            const topLeft = this.selectionHelper.pointTopLeft;
-            const left = (topLeft.x - canvas.left) / canvas.width * 2 - 1;
-            const top = - (topLeft.y - canvas.top) / canvas.height * 2 + 1;
-
-            const bottomRight = this.selectionHelper.pointBottomRight;
-            const right = (bottomRight.x - canvas.left) / canvas.width * 2 - 1;
-            const bottom = - (bottomRight.y - canvas.top) / canvas.height * 2 + 1;
-            console.debug(
-                'selectionHelper, top = %f, left = %f, bottom = %f, right = %f',
-                top, left, bottom, right,
-            );
-
-            // TODO: check the z-value of these points
-            this.selectionBox.startPoint.set(left, top, 0.5);
-            this.selectionBox.endPoint.set(right, bottom, 0.5);
-
-            // TODO: consider restricting selection to a specific object
-            const selection = this.selectionBox.select();
-            console.debug("selected points:", selection);
-
-            if (this.points && this.points.id in selection) {
-                const geometry = this.points.geometry as THREE.BufferGeometry;
-                const colors = geometry.getAttribute('color') as THREE.BufferAttribute;
-                const color = new THREE.Color(0xffffff);
-                for (const i of selection[this.points.id]) {
-                    colors.setXYZ(i, color.r, color.g, color.b);
-                }
-                colors.needsUpdate = true;
-            }
-        }
-    }
-
-    setSize(width: number, height: number) {
-        this.bloomPass.resolution.set(width, height);
-        this.renderer.setSize(width, height);
-        this.composer.setSize(width, height);
-    }
-
-    setSelecting(selecting: boolean) {
-        this.selectionHelper.enabled = selecting;
-        this.controls.enabled = !selecting;
-    }
-
-    initPointsGeometry(numPoints: number) {
-        const geometry = this.points.geometry;
-        if (!geometry.hasAttribute('position') || geometry.getAttribute('position').count !== numPoints) {
-            geometry.setAttribute(
-                'position',
-                new THREE.Float32BufferAttribute(new Float32Array(3 * numPoints), 3),
-            );
-            // prevent drawing uninitialized points at the origin
-            geometry.setDrawRange(0, 0)
-        }
-        if (!geometry.hasAttribute('color') || geometry.getAttribute('color').count !== numPoints) {
-            geometry.setAttribute(
-                'color',
-                new THREE.Float32BufferAttribute(new Float32Array(3 * numPoints), 3),
-            );
-        }
-        // Initialize all the colors immediately.
-        const color = new THREE.Color();
-        const colorAttribute = geometry.getAttribute('color');
-        for (let i = 0; i < numPoints; i++) {
-            const r = Math.random();
-            const g = Math.random();
-            const b = Math.random();
-            color.setRGB(r, g, b, THREE.SRGBColorSpace);
-            colorAttribute.setXYZ(i, color.r, color.g, color.b);
-        }
-        colorAttribute.needsUpdate = true;
-    }
-
-    setPointsPositions(data: Float32Array) {
-        const numPoints = data.length / 3;
-        const geometry = this.points.geometry;
-        const positions = geometry.getAttribute('position');
-        for (let i = 0; i < numPoints; i++) {
-            positions.setXYZ(i, data[3 * i], data[3 * i + 1], data[3 * i + 2]);
-        }
-        positions.needsUpdate = true;
-        geometry.setDrawRange(0, numPoints);
-        this.points.geometry.computeBoundingSphere();
-    }
-
-    dispose() {
-        this.renderer.domElement.removeEventListener('pointerup', this.pointerUp);
-        this.renderer.dispose();
-        this.points.geometry.dispose();
-        if (Array.isArray(this.points.material)) {
-            for (const material of this.points.material) {
-                material.dispose();
-            }
-        } else {
-            this.points.material.dispose();
-        }
-        this.selectionHelper.dispose();
-    }
- 
-}
 
 interface SceneProps {
     renderWidth: number;
@@ -235,7 +41,7 @@ export default function Scene(props: SceneProps) {
 
         // append renderer canvas
         const divCurrent = divRef.current;
-        const renderer = canvas.current.renderer;
+        const renderer = canvas.current!.renderer;
         divCurrent?.appendChild(renderer.domElement);
 
         const keyDown = (event: KeyboardEvent) => {
