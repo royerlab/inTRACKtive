748cde3da38b0b36cf3b2a3b191501edd22376b8
Ashley Anderson <aganders3@gmail.com>
2024-01-29 14:32:10 -0500

    Add react version detection in eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,9 @@ module.exports = {
     settings: {
         "import/resolver": {
             typescript: {}
-        }
-    }
+        },
+        "react": {
+            "version": "detect"
+        },
+    },
 };
