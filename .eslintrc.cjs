7e8b9580c7f27af5f8f151b82d33d5348ff1ed8d
Ashley Anderson <aganders3@gmail.com>
2024-01-29 14:09:30 -0500

    Add linting with ESLint + GitHub Actions Workflow

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+    root: true,
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-uses-react": "off",
+        "camelcase": "error",
+        "spaced-comment": "error",
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "no-duplicate-imports": "error",
+    },
+    settings: {
+        "import/resolver": {
+            typescript: {}
+        }
+    }
+};
