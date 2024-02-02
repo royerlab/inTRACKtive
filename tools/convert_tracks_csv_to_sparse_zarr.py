18e4a29294ec60098bf93a94f44761ecbf02c340
Ashley Anderson <aganders3@gmail.com>
2024-02-02 15:32:23 -0500

    Use lil instead of dok for faster sparse array construction. Fix chunk sizes.

--- tools/convert_tracks_csv_to_sparse_zarr.py
+++ tools/convert_tracks_csv_to_sparse_zarr.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 import numpy as np
 import zarr
-from scipy.sparse import dok_matrix
+from scipy.sparse import lil_matrix
 
 root_dir = "/Users/aandersoniii/Data/tracking/"
 
@@ -31,7 +31,7 @@ tracks = len(set(p[0] for p in points))
 
 # store the points in an array
 points_array = np.ones((timepoints, 3 * max_points_in_timepoint), dtype=np.float32) * -9999.9
-points_to_tracks = dok_matrix((timepoints * max_points_in_timepoint, tracks), dtype=np.int32)
+points_to_tracks = lil_matrix((timepoints * max_points_in_timepoint, tracks), dtype=np.int32)
 for point in points:
     track_id, t, z, y, x, n = point
     point_id = t * max_points_in_timepoint + n
@@ -50,6 +50,9 @@ points_to_tracks = points_to_tracks.tocsr()
 print(f"Converted to CSR in {time.monotonic() - start} seconds")
 start = time.monotonic()
 
+# TODO: tracks_to_points may want to store xyz for the points, not just the indices
+# this would make the indices array 3x (4x?) larger, but would eliminate the need to
+# fetch coordinates again based on point IDs
 tracks_to_points_zarr = zarr.hierarchy.group(
     zarr.storage.DirectoryStore(root_dir + "ZSNS001_tracks_to_points.zarr"),
     overwrite=True,
@@ -67,13 +70,12 @@ points_to_tracks_zarr.create_dataset("indices", data=points_to_tracks.indices)
 points_to_tracks_zarr.create_dataset("indptr", data=points_to_tracks.indptr)
 
 # save the points array (same format as ZSHS001_nodes.zarr)
-points_zarr = zarr.create(
-    points_array.shape,
-    chunks=tuple(np.ceil(s / 16) for s in points_array.shape),
+zarr.save_array(
+    zarr.storage.DirectoryStore(root_dir + "ZSNS001_points.zarr"),
+    points_array,
+    chunks=(1, points_array.shape[1]),
     dtype=np.float32,
 )
-points_zarr[:] = points_array
-zarr.save(zarr.storage.DirectoryStore(root_dir + "ZSNS001_points.zarr"), points_array)
 
 print(f"Saved to Zarr in {time.monotonic() - start} seconds")
 
