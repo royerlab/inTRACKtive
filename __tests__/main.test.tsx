1ffa5888386749b3132aa9a98388b30573d75e17
Ashley Anderson <aganders3@gmail.com>
2024-02-09 14:57:55 -0500

    More mocking - not sure why it passes locally and fails in CI

--- __tests__/main.test.tsx
+++ __tests__/main.test.tsx
@@ -7,15 +7,17 @@ import { render } from "@testing-library/react";
 // mock PointCanvas to avoid WebGL errors when testing without a browser
 vi.mock("../src/PointCanvas", () => {
     const PointCanvas = vi.fn();
-    PointCanvas.prototype.renderer = {
-        domElement: document.createElement("canvas"),
-    };
     PointCanvas.prototype.controls = {
         autoRotate: false,
         update: vi.fn(),
     };
+    PointCanvas.prototype.points = { id: 42 };
+    PointCanvas.prototype.renderer = { domElement: document.createElement("canvas") };
+
     PointCanvas.prototype.animate = vi.fn();
+    PointCanvas.prototype.dispose = vi.fn();
     PointCanvas.prototype.setSize = vi.fn();
+
     return { PointCanvas };
 });
 
