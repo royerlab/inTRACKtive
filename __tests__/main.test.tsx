f049e27d35a70cc071f4ad3af41ddd1e5215526d
Ashley Anderson <aganders3@gmail.com>
2024-05-20 20:46:28 -0400

    Fix test for new structure

--- __tests__/main.test.tsx
+++ __tests__/main.test.tsx
@@ -1,20 +1,31 @@
 import { expect, test } from "vitest";
 
-import { PointCanvas } from "../src/lib/PointCanvas.ts";
-import Scene from "../src/components/Scene";
+import * as THREE from "three";
 import React from "react";
 import { render } from "@testing-library/react";
+
+import { usePointCanvas } from "../src/hooks/usePointCanvas";
+import Scene from "../src/components/Scene";
 
 test("tests work", () => {
     expect(true).toBeTruthy();
 });
 
 test("render Scene", () => {
-    let canvasState: PointCanvas | null = null;
-    const setCanvas = (canvas: PointCanvas) => {
-        canvasState = canvas;
+    const TestContainer = () => {
+        const initialViewerState = {
+            cameraPosition: new THREE.Vector3(0, 0, 0),
+            cameraTarget: new THREE.Vector3(0, 0, 0),
+        };
+
+        const sceneDivRef = React.useRef<HTMLDivElement>(null);
+        const [_canvas, _dispatchCanvas] = usePointCanvas(sceneDivRef, initialViewerState);
+
+        return <Scene ref={sceneDivRef} loading={false} />;
     };
-    const { container } = render(<Scene setCanvas={setCanvas} loading={false} />);
+
+    const { container } = render(<TestContainer />);
     expect(container).not.toBeNull();
-    expect(canvasState).not.toBeNull();
+    expect(container.firstChild).not.toBeNull();
+    expect(container.firstChild?.firstChild).toBeInstanceOf(HTMLCanvasElement);
 });
