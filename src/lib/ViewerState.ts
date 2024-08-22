dd9a19318b915e53f1a7b1a9f3f44f4e15bb967c
TeunHuijben <teun.huijben@czbiohub.org>
2024-08-22 16:07:20 -0700

    centered points around origin of fov

--- src/lib/ViewerState.ts
+++ src/lib/ViewerState.ts
@@ -3,7 +3,8 @@
 //     "/points-web-viewer/sparse-zarr-v2/ZSNS001_tracks_bundle.zarr";
 
 // public Ascidian dataset that contains radius:
-export const DEFAULT_ZARR_URL = "https://public.czbiohub.org/royerlab/zoo/Ascidian/tracks_withSizee_bundle.zarr/";
+export const DEFAULT_ZARR_URL =
+    "https://public.czbiohub.org/royerlab/zoo/Ascidian/tracks_withSize_centered_bundle.zarr/";
 // export const DEFAULT_ZARR_URL = "https://public.czbiohub.org/royerlab/zoo/misc/tracks_withSize_3dots_bundle.zarr/";
 
 const HASH_KEY = "viewerState";
