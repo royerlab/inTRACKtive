111cca567ba279c35ec1558c3cd1a00152c6c856
Teun Huijben <45037215+TeunHuijben@users.noreply.github.com>
2024-09-05 12:12:21 -0700

    using dispatch for action on canvas
    Co-authored-by: Ashley Anderson <aganders3@gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -74,7 +74,7 @@ export default function App() {
         console.log("load data from %s", dataUrl);
         const trackManager = loadTrackManager(dataUrl);
         // TODO: add clean-up by returning another closure
-        canvas.removeAllTracks();
+        dispatchCanvas({ type: ActionType.REMOVE_ALL_TRACKS });
         trackManager.then((tm: TrackManager | null) => {
             setTrackManager(tm);
             // Defend against the case when a curTime valid for previous data
