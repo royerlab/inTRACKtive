294c8a588f527d407b81a58ed1c163b07504b89c
Teun Huijben <45037215+TeunHuijben@users.noreply.github.com>
2024-10-08 18:18:56 -0700

    restructured leftSidebar (#129)
    * restructured leftSidebar
    
    * more compact view, less white space
    
    * added divider between leftSidebar and ControlInstructions
    
    * lint fixes

--- src/components/leftSidebar/LeftSidebarWrapper.tsx
+++ src/components/leftSidebar/LeftSidebarWrapper.tsx
@@ -2,6 +2,15 @@ import { TrackManager } from "@/lib/TrackManager";
 import TrackControls from "./TrackControls";
 import ControlInstructions from "./ControlInstructions";
 import { PointSelectionMode } from "@/lib/PointSelector";
+import { PointCanvas } from "@/lib/PointCanvas";
+import { Divider } from "@mui/material";
+
+// interface Action {
+//     type: ActionType;
+//     brightness?: number;
+//     pointSize?: number;
+//     // Add more fields for other actions as needed
+// }
 
 interface LeftSidebarWrapperProps {
     hasTracks: boolean;
@@ -14,6 +23,9 @@ interface LeftSidebarWrapperProps {
     setTrackHighlightLength: (trackHighlightLength: number) => void;
     selectionMode: PointSelectionMode | null;
     isTablet: boolean;
+    canvas: PointCanvas;
+    setPointBrightness: (brightness: number) => void;
+    setPointSize: (pointSize: number) => void;
 }
 
 export default function LeftSidebarWrapper({
@@ -27,20 +39,27 @@ export default function LeftSidebarWrapper({
     setTrackHighlightLength,
     selectionMode,
     isTablet,
+    canvas,
+    setPointBrightness,
+    setPointSize,
 }: LeftSidebarWrapperProps) {
     return (
         <>
-            {hasTracks && (
-                <TrackControls
-                    trackManager={trackManager}
-                    trackHighlightLength={trackHighlightLength}
-                    showTracks={showTracks}
-                    setShowTracks={setShowTracks}
-                    showTrackHighlights={showTrackHighlights}
-                    setShowTrackHighlights={setShowTrackHighlights}
-                    setTrackHighlightLength={setTrackHighlightLength}
-                />
-            )}
+            <TrackControls
+                trackManager={trackManager}
+                trackHighlightLength={trackHighlightLength}
+                showTracks={showTracks}
+                setShowTracks={setShowTracks}
+                showTrackHighlights={showTrackHighlights}
+                setShowTrackHighlights={setShowTrackHighlights}
+                setTrackHighlightLength={setTrackHighlightLength}
+                pointBrightness={canvas.pointBrightness}
+                setPointBrightness={setPointBrightness}
+                pointSize={canvas.pointSize}
+                setPointSize={setPointSize}
+                hasTracks={hasTracks}
+            />
+            <Divider />
             {selectionMode !== null && <ControlInstructions selectionMode={selectionMode} isTablet={isTablet} />}
         </>
     );
