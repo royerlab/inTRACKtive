4443a076801ab4985300e7ea37871ca849e768e5
Kyle Awayan <kyle@awayan.com>
2024-10-10 13:43:33 -0700

    don't send url if zarr url is not on public.czbiohub.org

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -23,6 +23,28 @@ Sentry.init({
     ],
     // Tracing
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+    // For traces, only log request URLs if its from our domain
+    // (this is for when the user changes the zarr URL)
+    beforeSend(event) {
+        // If the event contains an HTTP request URL, inspect it
+        if (event.request && event.request.url) {
+            try {
+                // Create a URL object to inspect the request URL
+                const parsedUrl = new URL(event.request.url);
+                // Only allow events for the specific domain
+                if (parsedUrl.hostname !== "public.czbiohub.org") {
+                    // Return null to drop the event if the hostname does not match
+                    return null;
+                }
+            } catch (e) {
+                // In case of invalid URL or parsing errors, drop the event
+                return null;
+            }
+        }
+        // Otherwise, allow the event to be sent to Sentry
+        return event;
+    },
 });
 
 const domNode = document.getElementById("app")!;
