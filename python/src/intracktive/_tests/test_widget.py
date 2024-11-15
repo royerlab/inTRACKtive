c9d284c86d58ae58e665280e24a01ab9088bf712
TeunHuijben <teun.huijben@czbiohub.org>
2024-11-14 16:34:39 -0800

    pre-commit fix

--- python/src/intracktive/_tests/test_widget.py
+++ python/src/intracktive/_tests/test_widget.py
@@ -1,66 +1,64 @@
+import webbrowser
 from typing import Callable
 from unittest.mock import patch
 
 import napari
 import pytest
-import webbrowser
-
-from intracktive.widget import LauncherWidget
 from intracktive._tests.test_convert import make_sample_data
+from intracktive.widget import LauncherWidget
 
 
 def test_intracktive_widget_2D(
-        make_napari_viewer: Callable[[],napari.Viewer],
-        request,
-    ):
-
+    make_napari_viewer: Callable[[], napari.Viewer],
+    request,
+):
     df = make_sample_data()
-    filtered_df = df[df['parent_track_id'] != -1]
-    graph = dict(zip(filtered_df['track_id'], filtered_df['parent_track_id']))
+    filtered_df = df[df["parent_track_id"] != -1]
+    graph = dict(zip(filtered_df["track_id"], filtered_df["parent_track_id"]))
 
     viewer = make_napari_viewer()
     widget = LauncherWidget()
     viewer.window.add_dock_widget(widget)
-    viewer.add_tracks(df[['track_id','t','y','x']],graph=graph,name='Tracks')
-
+    viewer.add_tracks(df[["track_id", "t", "y", "x"]], graph=graph, name="Tracks")
 
     assert "Tracks" in viewer.layers
 
     assert widget._tracks_layer_w.value is not None
-    assert str(widget._file_dialog.value) is "."
+    assert str(widget._file_dialog.value) == "."
 
     # Attempt to simulate the "run" button click and catch errors
-    with patch.object(webbrowser, 'open', return_value=True) as mock_browser:
+    with patch.object(webbrowser, "open", return_value=True) as mock_browser:
         try:
             widget._run_btn_click()
+            mock_browser.assert_called_once()
         except Exception as e:
             pytest.fail(f"Button click failed with error: {e}")
 
     if request.config.getoption("--show-napari-viewer"):
         napari.run()
 
-def test_intracktive_widget_2D_withoutGraph(
-        make_napari_viewer: Callable[[],napari.Viewer],
-        request,
-    ):
 
+def test_intracktive_widget_2D_withoutGraph(
+    make_napari_viewer: Callable[[], napari.Viewer],
+    request,
+):
     df = make_sample_data()
 
     viewer = make_napari_viewer()
     widget = LauncherWidget()
     viewer.window.add_dock_widget(widget)
-    viewer.add_tracks(df[['track_id','t','y','x']],name='Tracks')
-
+    viewer.add_tracks(df[["track_id", "t", "y", "x"]], name="Tracks")
 
     assert "Tracks" in viewer.layers
 
     assert widget._tracks_layer_w.value is not None
-    assert str(widget._file_dialog.value) is "."
+    assert str(widget._file_dialog.value) == "."
 
     # Attempt to simulate the "run" button click and catch errors
-    with patch.object(webbrowser, 'open', return_value=True) as mock_browser:
+    with patch.object(webbrowser, "open", return_value=True) as mock_browser:
         try:
             widget._run_btn_click()
+            mock_browser.assert_called_once()
         except Exception as e:
             pytest.fail(f"Button click failed with error: {e}")
 
@@ -69,29 +67,28 @@ def test_intracktive_widget_2D_withoutGraph(
 
 
 def test_intracktive_widget_3D(
-        make_napari_viewer: Callable[[],napari.Viewer],
-        request,
-    ):
-
+    make_napari_viewer: Callable[[], napari.Viewer],
+    request,
+):
     df = make_sample_data()
-    filtered_df = df[df['parent_track_id'] != -1]
-    graph = dict(zip(filtered_df['track_id'], filtered_df['parent_track_id']))
+    filtered_df = df[df["parent_track_id"] != -1]
+    graph = dict(zip(filtered_df["track_id"], filtered_df["parent_track_id"]))
 
     viewer = make_napari_viewer()
     widget = LauncherWidget()
     viewer.window.add_dock_widget(widget)
-    viewer.add_tracks(df[['track_id','t','z','y','x']],graph=graph,name='Tracks')
-
+    viewer.add_tracks(df[["track_id", "t", "z", "y", "x"]], graph=graph, name="Tracks")
 
     assert "Tracks" in viewer.layers
 
     assert widget._tracks_layer_w.value is not None
-    assert str(widget._file_dialog.value) is "."
+    assert str(widget._file_dialog.value) == "."
 
     # Attempt to simulate the "run" button click and catch errors
-    with patch.object(webbrowser, 'open', return_value=True) as mock_browser:
+    with patch.object(webbrowser, "open", return_value=True) as mock_browser:
         try:
             widget._run_btn_click()
+            mock_browser.assert_called_once()
         except Exception as e:
             pytest.fail(f"Button click failed with error: {e}")
 
@@ -100,21 +97,21 @@ def test_intracktive_widget_3D(
 
 
 def test_intracktive_widget_noTracksLayer(
-        make_napari_viewer: Callable[[],napari.Viewer],
-        request,
-    ):
-
+    make_napari_viewer: Callable[[], napari.Viewer],
+    request,
+):
     viewer = make_napari_viewer()
     widget = LauncherWidget()
     viewer.window.add_dock_widget(widget)
 
     assert widget._tracks_layer_w.value is None
-    assert str(widget._file_dialog.value) is "."
+    assert str(widget._file_dialog.value) == "."
 
     # Attempt to simulate the "run" button click and catch errors
-    with patch.object(webbrowser, 'open', return_value=True) as mock_browser:
+    with patch.object(webbrowser, "open", return_value=True) as mock_browser:
         try:
             widget._run_btn_click()
+            mock_browser.assert_called_once()
         except Exception as e:
             pytest.fail(f"Button click failed with error: {e}")
 
