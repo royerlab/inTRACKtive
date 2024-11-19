6fcb7bde040922703a9a2c83efc67fcf564a0370
TeunHuijben <teun.huijben@czbiohub.org>
2024-11-19 11:14:48 -0800

    added CORS options to http server to prevent error in Chrome

--- python/src/intracktive/server.py
+++ python/src/intracktive/server.py
@@ -24,6 +24,15 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
 
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "*")
+        super().end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "*")
         super().end_headers()
 
 
