f9fa9973fd0c519df27d2a59c6329f4288c67e72
TeunHuijben <teun.huijben@czbiohub.org>
2024-12-03 14:14:49 -0800

    demo ready PR for SI videos (removed some default attributes)

--- src/components/leftSidebar/DynamicDropdown.tsx
+++ src/components/leftSidebar/DynamicDropdown.tsx
@@ -21,8 +21,8 @@ const DEFAULT_DROPDOWN_OPTIONS: Option[] = [
     { name: "x-position", label: 1, type: "continuous", action: "calculate", numCategorical: undefined },
     { name: "y-position", label: 2, type: "continuous", action: "calculate", numCategorical: undefined },
     { name: "z-position", label: 3, type: "continuous", action: "calculate", numCategorical: undefined },
-    { name: "sign(x-pos)", label: 4, type: "categorical", action: "calculate", numCategorical: 2 },
-    { name: "quadrants", label: 5, type: "categorical", action: "calculate", numCategorical: 8 },
+    // { name: "sign(x-pos)", label: 4, type: "categorical", action: "calculate", numCategorical: 2 },
+    // { name: "quadrants", label: 5, type: "categorical", action: "calculate", numCategorical: 8 },
 ];
 
 export const numberOfDefaultColorByOptions = DEFAULT_DROPDOWN_OPTIONS.length;
