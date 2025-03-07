7863514396bc3ed24498f9ae8d10e4cf8da562ec
TeunHuijben <teun.huijben@czbiohub.org>
2025-03-07 14:48:08 -0800

    revert back to zebrafish default dataset

--- CONFIG.ts
+++ CONFIG.ts
@@ -11,8 +11,7 @@ const config = {
     // When opening the viewer, or refreshing the page, the viewer will revert to the following default dataset
     data:{
         // Default dataset URL (must be publically accessible)
-        // default_dataset: "https://public.czbiohub.org/royerlab/zoo/Zebrafish/tracks_zebrafish_bundle.zarr/"
-        default_dataset: "https://public.czbiohub.org/royerlab/zoo/C_elegans/tracks_elegans_bundle.zarr/"
+        default_dataset: "https://public.czbiohub.org/royerlab/zoo/Zebrafish/tracks_zebrafish_bundle.zarr/"
     },
   
     // Default settings for certain parameters
