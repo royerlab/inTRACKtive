89385669c7f3ce8bc88ff07eea1045739e79882a
TeunHuijben <teun.huijben@czbiohub.org>
2025-03-19 15:55:41 -0700

    better cell size scaling when window resizes

--- CONFIG.ts
+++ CONFIG.ts
@@ -11,7 +11,8 @@ const config = {
     // When opening the viewer, or refreshing the page, the viewer will revert to the following default dataset
     data:{
         // Default dataset URL (must be publically accessible)
-        default_dataset: "https://public.czbiohub.org/royerlab/zoo/Zebrafish/tracks_zebrafish_bundle.zarr/"
+        // default_dataset: "https://public.czbiohub.org/royerlab/zoo/Zebrafish/tracks_zebrafish_bundle.zarr/"
+        default_dataset: "https://public.czbiohub.org/royerlab/zoo/Ascidian/tracks_ascidian_attributes_bundle.zarr/"
     },
   
     // Default settings for certain parameters
