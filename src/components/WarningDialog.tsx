31a33dc7dcffd1e981afcde99ea15aa29c43e5fb
TeunHuijben <teun.huijben@czbiohub.org>
2025-04-21 14:06:02 -0700

    try removing box from WarningDialog

--- src/components/WarningDialog.tsx
+++ src/components/WarningDialog.tsx
@@ -1,4 +1,4 @@
-import { Box, Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
 
 interface WarningDialogProps {
     open: boolean;
@@ -9,19 +9,17 @@ interface WarningDialogProps {
 
 export default function WarningDialog(props: WarningDialogProps) {
     return (
-        <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "2em" }}>
-            <Dialog open={props.open} onClose={props.onCloseAction}>
-                <DialogTitle>Warning</DialogTitle>
-                <DialogContent>
-                    {`You have selected ${props.numUnfetchedPoints} new cells, which might take a long time to load. Continue?`}
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={props.onCloseAction} color="primary">
-                        Cancel
-                    </Button>
-                    <Button onClick={props.onContinueAction}>Continue</Button>
-                </DialogActions>
-            </Dialog>
-        </Box>
+        <Dialog open={props.open} onClose={props.onCloseAction}>
+            <DialogTitle>Warning</DialogTitle>
+            <DialogContent>
+                {`You have selected ${props.numUnfetchedPoints} new cells, which might take a long time to load. Continue?`}
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.onCloseAction} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={props.onContinueAction}>Continue</Button>
+            </DialogActions>
+        </Dialog>
     );
 }
