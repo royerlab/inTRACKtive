65c4aba0a8c1673e4bd85bb003f72ee53954faa6
Teun Huijben <teun.huijben@czbiohub.org>
2025-05-23 17:03:38 -0700

    update notebook_widget.py

--- python/src/intracktive/notebook_widget.py
+++ python/src/intracktive/notebook_widget.py
@@ -14,14 +14,22 @@ class Widget(anywidget.AnyWidget):
     ).tag(sync=True)
     selected_cells = traitlets.List().tag(sync=True)
 
-    def select_tracks(self, cell_ids):
+    def select_tracks(self, track_ids):
+        # Ensure cell_ids is a normal list with values
+        if (
+            not isinstance(track_ids, list)
+            or not track_ids
+            or not all(isinstance(x, (int, float)) for x in track_ids)
+        ):
+            raise ValueError("track_ids must be a non-empty list of numbers")
+
         # Convert from 1-based (Python) to 0-based (JavaScript) indexing
-        adjusted_ids = [id - 1 for id in cell_ids]
+        adjusted_ids = [id - 1 for id in track_ids]
         self.selected_cells = adjusted_ids
 
     @property
     def get_selected_tracks(self):
-        # Convert from 0-based (JavaScript) to 1-based (Python) indexing
+        # Convert track_ids from 0-based (JavaScript) to 1-based (Python) indexing
         return [id + 1 for id in self._internal_selected_tracks]
 
     # Reference the static files relative to this package
